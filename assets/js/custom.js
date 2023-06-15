312e29eb4a8fb27d31a59bec270ee9bb1e66ede9
sanfyin <sanjibdas86@gmail.com>
2023-06-15 07:33:30 +0530

    Updated logo and about SJC

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,7 +42,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2023/08/15').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2023/10/14').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
